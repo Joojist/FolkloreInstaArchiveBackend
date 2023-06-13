88dbe454d68fc1c4c52786bcc1b04c06537d967f
stevenmaasi <zteven@tlu.ee>
2023-06-13 22:22:27 +0300

    test

--- bootstrap.php
+++ bootstrap.php
@@ -9,7 +9,7 @@ use App\Entity\Post;
 
 require_once "vendor/autoload.php";
 
-// Create a simple "default" Doctrine ORM configuration
+// Create a simple "default" Doctrine ORM configuration test
 $isDevMode = true;
 $entitiesPath = [__DIR__."/src"];
 $dbParams = include 'config/database.php';
